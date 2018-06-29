138911c695ba235b3ed1de854265cde19ff6f3ae
afraquadri <afraq1@hotmail.com>
2018-06-29 14:13:11 -0500

    Add existing file

---
+++ Finding Duplicates with CTE.sql
@@ -0,0 +1,27 @@
+
+
+
+SELECT JOB,COUNT(JOB) FROM EMP GROUP BY JOB;
+
+
+
+
+
+SELECT OrderNo, shoppername, amountPayed, city, item, count(*) as cnt
+FROM dbo.sales
+GROUP BY OrderNo, shoppername, amountPayed, city, item
+HAVING COUNT(*) > 1
+
+
+
+
+
+with x as   (select  *,rn = row_number()
+            over(PARTITION BY OrderNo,item  order by OrderNo)
+            from    #temp1)
+select * from x
+--delete x 
+where rn > 1
+
+
+
